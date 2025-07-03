f1013e6d29e01e69d67e03829197e5d04f4f2e9f
ehddus20201 <ehddus20201@naver.com>
2025-07-03 14:04:44 +0900

    [TS] supabase 기본 설정

--- memo-app/src/service/service.ts
+++ memo-app/src/service/service.ts
@@ -1,9 +1,13 @@
 import { renderMemo, type MemoData } from "../card";
 import { data } from "../data";
 import { main } from "../main";
+import { supabase } from "../supabase/supabase";
 
 export function fetchMemo(){
   data.forEach((d:MemoData)=>{
     renderMemo(main,d);
   })
 }
+
+
+console.log(supabase);
